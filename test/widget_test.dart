d839f8b975b4c4865e5c67652182bdef96dfc84b
Andy Wendt <awendt@mavenlink.com>
2019-12-02 20:54:42 -0700

    Fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_app/my_app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Main Test', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-    expect(find.text('Welcome to Flutter'), findsOneWidget);
+    expect(find.text('Startup Name Generator'), findsOneWidget);
   });
 }
